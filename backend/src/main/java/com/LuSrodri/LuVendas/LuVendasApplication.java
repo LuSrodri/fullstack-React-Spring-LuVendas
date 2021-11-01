84758e4e68cc69bb759de38ce6d893cf8c291ad5
LuSrodri <lucassrodri08@gmail.com>
2021-11-01 18:09:55 -0300

    Project created

---
+++ backend/src/main/java/com/LuSrodri/LuVendas/LuVendasApplication.java
@@ -0,0 +1,13 @@
+package com.LuSrodri.LuVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuVendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/LuSrodri/LuVendas/LuVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LuSrodri.LuVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
