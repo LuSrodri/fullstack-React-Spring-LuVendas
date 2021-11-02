c2b932f40fb918d2b3f4051702fcf3bdb5018ad4
LuSrodri <lucassrodri08@gmail.com>
2021-11-02 11:01:20 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+
+
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/LuSrodri" target="_blank" rel="noreferrer">Lucas Santos Rodrigues</a></p>
+                <p className="text-light"><small><strong>LuVendas, projeto de site de vendas usando Spring e React </strong><br /></small></p>
+            </div>
+        </footer>
+
+
+    );
+}
+
+export default Footer;
