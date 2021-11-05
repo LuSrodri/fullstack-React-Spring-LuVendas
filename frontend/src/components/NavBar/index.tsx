381f6c3e21dce076ab802779efe4a2db95ef6c7e
LuSrodri <lucassrodri08@gmail.com>
2021-11-05 19:08:02 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 
 function NavBar() {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
-                    <h4>Lu<span className="text-primary">Vendas</span></h4>
+                    <Link to="/" style={{ textDecoration: 'none', color: "black"}} ><h4 >Lu<span className="text-primary">Vendas</span></h4></Link>
                 </nav>
             </div>
         </div>
